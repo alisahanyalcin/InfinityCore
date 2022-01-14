a324d514453ff0784d9c763316ffd7b0775a1430
alisahanyalcin <alisahanyalcin4@gmail.com>
2022-01-14 17:33:56 +0300

    Changed the BaseController logic

--- core/BaseController.php
+++ core/BaseController.php
@@ -4,10 +4,22 @@ namespace InfinityCore\Core;
 
 class BaseController
 {
+    /**
+     * @var Application $app
+     */
     protected Application $app;
+    /**
+     * @var BaseView $view
+     */
     protected BaseView $view;
+    /**
+     * @var PDOx\PDOx $PDOx
+     */
     protected PDOx\PDOx $PDOx;
 
+    /**
+     * BaseController constructor.
+     */
     public function __construct()
     {
         $this->app = new Application();
